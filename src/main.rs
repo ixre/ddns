e422c5fb085b3531d9c73235e93fc1bf44198d6f
jarryliu <jarrysix@gmail.com>
2018-10-23 23:06:10 +0800

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 extern crate ddns;
 
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, ddns!");
+    let (tx, rx) = mpsc::channel();
+    let h = thread::spawn(move || {
+        loop{
+            tx.send(1).unwrap(); // 可写多次
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    // 单次接收
+    // println!("value = {}", rx.recv().unwrap());
+    for v in rx{ println!("value = {}",v );}
+    h.join().unwrap();
 }

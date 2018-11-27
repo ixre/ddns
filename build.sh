4c066d0de67b0d7c242ba1b661f327183f79e600
jarryliu <jarrysix@gmail.com>
2018-11-27 15:28:09 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#docker pull ekidd/rust-musl-builder
+#docker run --rm -it -v $(pwd):/home/rust/src ekidd/rust-musl-builder \
+#cargo build --release --target x86_64-unknown-linux-musl
+
+version="1.0"
+target=$(pwd)/dist/ddns
+rm -rf dist && mkdir -p ${target}
+cp ddns_default.conf ${target}/ddns.conf
+cp target/x86_64-unknown-linux-musl/release/ddns ${target}
+cd ${target}/../ && tar cvzf ddns-linux-${version}-x86_64.tar.gz ddns
+cd - > /dev/null
